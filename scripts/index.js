1b0fc9489fe104d468d5a4edb742920720db78f3
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2023-01-12 00:12:51 +0300

    fix: fixed code html, CSS, JS - third review v.3.1 11.01.2023

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,34 @@
 let popup = document.querySelector('.popup'); //страница модалки
 let open_popup = document.querySelector('.profile__edit-button'); //кнопка открытия модалки
 let close_popup = document.querySelector('.popup__close-button'); //крестик - кнопка закрытия модалки
-let addButton = document.querySelector('.popup__add-button'); //кнопка сохранить в модалке
 let nameTitle = document.querySelector('.profile__title'); //поле имени на главной странице
 let job = document.querySelector('.profile__subtitle'); //поле профессии на главной странице
 let formElement = document.querySelector('.popup__form'); //обёртка формы в HTML
-let nameInput = formElement.querySelector('.popup__input-name'); //поле ввода имени в модалке
-let jobInput = formElement.querySelector('.popup__input-job'); //поле ввода професии в модалке
+let nameInput = formElement.querySelector('.popup__input_type_name'); //поле ввода имени в модалке
+let jobInput = formElement.querySelector('.popup__input_type_job'); //поле ввода професии в модалке
 
 
 //open popup
-function openPopup () {    
-    popup.classList.add('popup_opened');
-    nameInput.value = nameTitle.textContent;
-    jobInput.value = job.textContent;
+function openPopup () {
+  nameInput.value = nameTitle.textContent;
+  jobInput.value = job.textContent;
+  popup.classList.add('popup_opened');
 }
 open_popup.addEventListener('click', openPopup);
 
 //close popup
 function closePopup () {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 close_popup.addEventListener('click', closePopup);
 
 
 //input form
 function handleFormSubmit (evt) {
-    evt.preventDefault();
+  evt.preventDefault();
+  job.textContent = jobInput.value;
+  nameTitle.textContent = nameInput.value;
 
-    nameTitle.textContent = nameInput.value;
-    job.textContent = jobInput.value;
-
-    closePopup ();
+  closePopup ();
 }
 formElement.addEventListener('submit', handleFormSubmit);
